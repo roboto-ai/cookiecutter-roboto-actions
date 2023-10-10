c495332ebdb877709c7648f1e03130f362b91f73
Gabe Medrash <gmedrash@gmail.com>
2023-10-10 13:15:18 -0700

    Use docker buildx if available

--- cookiecutter-roboto-python-action/{{cookiecutter.__package_name}}/scripts/build.sh
+++ cookiecutter-roboto-python-action/{{cookiecutter.__package_name}}/scripts/build.sh
@@ -5,4 +5,10 @@ set -euo pipefail
 SCRIPTS_ROOT=$( cd -- "$( dirname -- "${BASH_SOURCE[0]}" )" &> /dev/null && pwd)
 PACKAGE_ROOT=$(dirname "${SCRIPTS_ROOT}")
 
-docker build -f $PACKAGE_ROOT/Dockerfile -t {{ cookiecutter.__package_name }}:latest $PACKAGE_ROOT
+build_subcommand=(build)
+# if buildx is installed, use it
+if docker buildx version &> /dev/null; then
+    build_subcommand=(buildx build --platform linux/amd64 --output type=image)
+fi
+
+docker "${build_subcommand[@]}" -f $PACKAGE_ROOT/Dockerfile -t {{ cookiecutter.__package_name }}:latest $PACKAGE_ROOT
