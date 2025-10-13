70eadb12b13a8b0141b8688abd0247a6e442367e
GabeMedrash <gabe@roboto.ai>
2025-10-13 16:52:21 -0700

    Remove 'roboto-generic-option' template

--- cookiecutter-roboto-generic-action/hooks/post_gen_project.sh
+++
@@ -1,9 +0,0 @@
-#!/usr/bin/env bash
-
-set -euo pipefail
-
-{% if cookiecutter.initialize_git_repo %}
-git init --quiet --initial-branch=main
-git add .
-git commit -m "Initialize from project template"
-{% endif %}
